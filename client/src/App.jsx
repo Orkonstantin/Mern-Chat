9a407d060296aca80188bc677e207d5d96ce60be
or konstantin <104775108+Orkonstantin@users.noreply.github.com>
2023-05-25 06:21:42 +0300

    Vercel 2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,8 @@ import axios from "axios";
 import {UserContextProvider} from "./UserContext.jsx";
 import Routes from "./Routes.jsx";
 function App() {
-  axios.defaults.baseURL = 'http://localhost:4040';
+  // axios.defaults.baseURL = 'http://localhost:4040';
+    axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
   axios.defaults.withCredentials = true;
   return (
       <UserContextProvider>
